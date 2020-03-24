bfbfb3ae4bf6c65e7307c4723f4a83fae1242d75
William Li <williamli@bbi.io>
2020-03-24 20:30:02 +0800

    use dummy example for api

---
+++ api/endpoints/login.js
@@ -0,0 +1,28 @@
+// import { json, send, createError, run } from 'micro'
+// import { IncomingMessage, ServerResponse } from 'http'
+
+// // import { auth } from '../../lib/authenticate'
+
+// const loginHandler = async (req: IncomingMessage, res: ServerResponse) => {
+//   // TODO: Fix the any here
+//   const userCreds: any = await json(req)
+
+//   try {
+//     const token = await auth(userCreds)
+//     res.setHeader('Authorization', `Bearer ${token}`)
+//     send(res, 200, { token: token })
+//   } catch (error) {
+//     console.log(error)
+//     throw createError(error.statusCode, error.statusText)
+//   }
+// }
+
+// export default async (req: any, res: any) => run(req, res, loginHandler)
+export default (req, res) => {
+  const date = new Date()
+    .toISOString()
+    .replace(/T/, ' ')
+    .replace(/\..+/, '');
+
+  res.json({ date });
+};
